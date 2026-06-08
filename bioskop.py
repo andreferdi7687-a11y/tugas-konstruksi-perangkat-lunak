6d0bfa4027aeb67e1aaa15fa8de36ee3497b783d
Andre Ferdi <email_andre@gmail.com>
2026-06-08 16:57:03 +0700

    refactor: replace magic numbers with descriptive constants

--- bioskop.py
+++ bioskop.py
@@ -1,14 +1,20 @@
+HARGA_WEEKDAY = 50000
+HARGA_WEEKEND = 70000
+
+MINIMAL_TIKET_DISKON = 3
+PERSENTASE_DISKON_MEMBER = 0.10
+
 def hitung_pemesanan_tiket(jumlah_tiket, hari_nonton, status_member):
-    # Mengubah nama fungsi dan variabel menjadi lebih deskriptif sesuai PEP 8
+    # Mengganti angka mentah dengan konstanta deskriptif
     if hari_nonton.upper() in ["SABTU", "MINGGU"]:
-        harga_normal = jumlah_tiket * 70000
+        harga_normal = jumlah_tiket * HARGA_WEEKEND
     else:
-        harga_normal = jumlah_tiket * 50000
+        harga_normal = jumlah_tiket * HARGA_WEEKDAY
         
     potongan_diskon = 0
     if status_member:
-        if jumlah_tiket >= 3:
-            potongan_diskon = harga_normal * 0.10
+        if jumlah_tiket >= MINIMAL_TIKET_DISKON:
+            potongan_diskon = harga_normal * PERSENTASE_DISKON_MEMBER
             
     total_bayar = harga_normal - potongan_diskon
     
